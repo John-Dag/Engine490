4ed3a5e1c49969ca028cc706fca5b8ff700f1a71
JD <john.j.daglas@gmail.com>
2014-09-02 18:01:40 -0700

    Test

---
+++ android/src/com/gdx/engine/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gdx.engine.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gdx.engine.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/com/gdx/engine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.engine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.engine.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
