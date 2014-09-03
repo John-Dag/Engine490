38ba5a5adcd2b3703702a10294171dfc78bd3a35
JD <john.j.daglas@gmail.com>
2014-09-03 14:10:51 -0700

    Renamed Game class

--- android/src/com/gdx/engine/android/AndroidLauncher.java
+++ android/src/com/gdx/engine/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.gdx.engine.Game;
+import com.gdx.engine.Engine;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Game(), config);
+		initialize(new Engine(), config);
 	}
 }
