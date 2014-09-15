744170fc466c3c685e28f02e77c294b48d6ef41e
John Daglas <john.j.daglas@gmail.com>
2014-09-15 12:12:52 -0700

    Implementing input

--- core/src/com/gdx/engine/GameScreen.java
+++ core/src/com/gdx/engine/GameScreen.java
@@ -2,6 +2,7 @@ package com.gdx.engine;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
@@ -19,12 +20,13 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 public class GameScreen implements Screen {
 	public Game game;
 	public Level level;
-	public PerspectiveCamera camera;
+	public static PerspectiveCamera camera;
 	public CameraInputController cameraController;
 	public ModelBatch modelBatch;
 	public SpriteBatch spriteBatch;
@@ -32,9 +34,10 @@ public class GameScreen implements Screen {
 	public Model box;
 	public Environment environment;
 	public Array<ModelInstance> instances = new Array<ModelInstance>();
-	public boolean loading;
 	public BitmapFont bitmapFont;
 	public World world;
+	public Vector2 center;
+	public boolean loading;
 	
 	public GameScreen(Game game) {
 		this.game = game;
@@ -57,6 +60,7 @@ public class GameScreen implements Screen {
 		spriteBatch = new SpriteBatch();
 		bitmapFont = new BitmapFont();
 		Gdx.input.setInputProcessor(cameraController);
+		center = new Vector2(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 
 		loading = true;
 	}
@@ -71,14 +75,14 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		if (loading && Assets.manager.update()) {
-			System.out.println("Assets successfully loaded.");
+			System.out.println("Assets loaded.");
 			doneLoading();
 		}
 		camera.position.set(world.player.position);
 		camera.update();
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		
+		level.instances.get(0).transform.setToTranslation(world.player.position.x, world.player.position.y, world.player.position.z + 2.0f);
 		//modelBatch.begin(camera);
 		//modelBatch.render(instances, environment);
 		//modelBatch.end();
@@ -92,6 +96,20 @@ public class GameScreen implements Screen {
 	}
 	
 	public void updateInput() {
+		//MOUSE INPUT
+		if (Gdx.input.justTouched()) {
+			Vector2 deltaPos = new Vector2(Gdx.input.getX() - center.x, Gdx.input.getY() - center.y);
+			
+			boolean rotX = deltaPos.x != 0;
+			//boolean rotY = deltaPos.y != 0;
+			
+			System.out.println(" " + (int)deltaPos.x);
+			
+			if (rotX)
+				Gdx.input.setCursorPosition((int)center.x, (int)center.y);
+		}
+			
+		//KEYBOARD INPUT
 		if (Gdx.input.isKeyPressed(Keys.D))
 			world.player.position.x -= 0.05f;
 		if (Gdx.input.isKeyPressed(Keys.A))
--- core/src/com/gdx/engine/World.java
+++ core/src/com/gdx/engine/World.java
@@ -1,10 +1,14 @@
 package com.gdx.engine;
 
+import com.badlogic.gdx.utils.Array;
+
 public class World {
 	public Player player;
+	public Array<Player> playerInstances = new Array<Player>();
 	
 	public World() {
 		player = new Player(2f, 0f, 0f, true);
+		playerInstances.add(player);
 	}
 	
 	public void update() {
