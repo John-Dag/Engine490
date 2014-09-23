45a7dd0ef6bd2d757be022bc10e380b3df4e33e3
JD <john.j.daglas@gmail.com>
2014-09-22 21:17:49 -0700

    Testing

--- desktop/src/com/gdx/engine/desktop/DesktopLauncher.java
+++ desktop/src/com/gdx/engine/desktop/DesktopLauncher.java
@@ -3,12 +3,13 @@ package com.gdx.engine.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gdx.engine.Engine;
-import com.gdx.engine.MeshCreatorTest;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Engine 490";
+		config.width = 960;
+		config.height = 540;
 		new LwjglApplication(new Engine(), config);
-		//new LwjglApplication(new MeshCreatorTest(), config);
 	}
 }
