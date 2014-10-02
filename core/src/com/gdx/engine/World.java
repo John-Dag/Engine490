0d5c7c691cd20be858d7ac488febb399c1aea8e9
JD <john.j.daglas@gmail.com>
2014-10-01 21:42:09 -0700

    Added ray picking for entities.

--- core/src/com/gdx/engine/World.java
+++ core/src/com/gdx/engine/World.java
@@ -16,17 +16,21 @@ public class World {
 	//private Level level;
 	private MeshLevel meshLevel;
 	private Ray ray;
-	private Array<Decal> decalInstances = new Array<Decal>();
-	private Array<BoundingBox> boxes = new Array<BoundingBox>();
+	private Array<Decal> decalInstances;
+	private Array<BoundingBox> boxes;
 	private float timer;
-	private Vector3 position = new Vector3();
-	private Vector3 out = new Vector3();
+	private Vector3 position;
+	private Vector3 out;
     //private TiledMapTileLayer layer = (TiledMapTileLayer)Assets.level2.getLayers().get(0);
 	
 	public World() {
 		player = new Player(this, new Vector3(2f, 1.5f, 2f), true, BuildModel.buildBoxColorModel(1f, 1f, 1f, Color.BLUE));
-		meshLevel = new MeshLevel(Assets.level2, false);
+		meshLevel = new MeshLevel(Assets.level2, true);
 		//meshLevel.getInstances().add(player.model);
+		decalInstances = new Array<Decal>();
+		boxes = new Array<BoundingBox>();
+		position = new Vector3();
+		out = new Vector3();
 	}
 	
 	public void update(float delta) {
@@ -44,7 +48,7 @@ public class World {
 			Entity entity = meshLevel.getEntityInstances().get(i);
 			
 			if (entity.active) {
-				//entity.UpdateInstanceTransform();
+				entity.UpdateInstanceTransform();
 			}
 			
 			else {
@@ -78,25 +82,20 @@ public class World {
 		}
 	}
 	
-	public void rayPicking() {
-		ray = null;
-		if (player.mouseLeft == true && timer >= 0.1f) {
-			ray = player.camera.getPickRay(Gdx.input.getX(), Gdx.input.getY());
-			
-		}
-	}
-	
-	//Temporary
 	public void rayPick() {
-		ray = null;
-		int size = meshLevel.getInstances().size;
-		int result = -1;
-		float distance = -1;
-
 		if (player.mouseLeft == true && timer >= 0.1f) {
 			ray = player.camera.getPickRay(Gdx.input.getX(), Gdx.input.getY());
 			timer = 0;
+			rayPickLevel();
+			rayPickEntities();
+			setDecals();
 		}
+	}
+	
+	public void rayPickLevel() {
+		int size = meshLevel.getInstances().size;
+		int result = -1;
+		float distance = -1;
 		
 		if (ray != null) {
 			for (int i = 0; i < size; i++) {
@@ -114,18 +113,47 @@ public class World {
 					distance = dist2;
 					
 					if (result > -1) {
-						System.out.println("hit");
 						Intersector.intersectRayBounds(ray, boxes.get(i), out);
 					}
 				}
 			}
-			
-			Decal test = Decal.newDecal(Assets.test1, true);
-			test.setPosition(out);
-			test.lookAt(player.camera.position, player.camera.position.cpy().nor());
-			test.setScale(0.001f);
-			decalInstances.add(test);
 		}
+	}
+	
+	public void rayPickEntities() {
+		int size = meshLevel.getEntityInstances().size;
+		int result = -1;
+		float distance = -1;
+		
+		if (ray != null) {
+			for (int i = 0; i < size; i++) {
+				Entity entity = meshLevel.getEntityInstances().get(i);
+				
+				entity.model.transform.getTranslation(position);
+				position.add(entity.boundingBox.getCenter());
+				float dist2 = ray.origin.dst2(position);
+				
+				if (distance >= 0f && dist2 > distance)
+					continue;
+		
+				if (Intersector.intersectRayBoundsFast(ray, entity.boundingBox)) {
+					result = i;
+					distance = dist2;
+					
+					if (result > -1) {
+						Intersector.intersectRayBounds(ray, entity.boundingBox, out);
+					}
+				}
+			}
+		}
+	}
+	
+	public void setDecals() {
+		Decal decal = Decal.newDecal(Assets.test1, true);
+		decal.setPosition(out);
+		decal.lookAt(player.camera.position, player.camera.position.cpy().nor());
+		decal.setScale(0.001f);
+		decalInstances.add(decal);
 	}
 	
 	public Player getPlayer() {
